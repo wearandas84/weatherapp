3cd745aee64a1f9bfa0f42c8c5b81fe995311cc9
William Aranda <wearandas.84@outlook.com>
2023-02-26 16:13:13 -0600

    Se agrega la funcionalidad

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 'use strict'
-getData();
+
+const btn = document.querySelector("#envia");
+
+btn.addEventListener("click",getData,false);
+
+function clearInp(){
+    document.querySelector("#ciudad").value="";
+    document.querySelector("#estado").value="";
+    document.querySelector("#pais").value="";
+}
+
+function clearWeather(){
+    document.querySelector("#city").textContent="Ciudad: ";
+    document.querySelector("#conds").textContent="Condiciones: ";
+    document.querySelector("#temp").textContent="Temperatura: ";
+    document.querySelector("#tmax").textContent="Maxima: ";
+    document.querySelector("#tmin").textContent="Minima: ";
+}
 
 async function getData(){
-    var ciudad = "Mérida";
-    var estado = "Yucatán";
-    var pais = "MX";
+    document.querySelector(".info").style.visibility = "visible";
+    clearWeather();
+    var ciudad = document.querySelector("#ciudad").value;
+    var estado = document.querySelector("#estado").value;
+    var pais = document.querySelector("#pais").value;
+
     const apikey = "6f1c1ac1863908f7bd57c53184083962";
-    const geo = "http://api.openweathermap.org/geo/1.0/direct?q="+ciudad+","+estado+","+pais+"&limit=1&appid="+apikey;
+    var geo = "http://api.openweathermap.org/geo/1.0/direct?q="+ciudad+","+estado+","+pais+"&limit=1&appid="+apikey;
     var url="";
 
     fetch(geo)
@@ -19,7 +39,6 @@ async function getData(){
             }
         })
         .then((data) => {
-            console.log(data);
             var lat = data[0]["lat"];
             var lon = data[0]["lon"];
             url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apikey+"&lang=es&units=metric";
@@ -44,6 +63,7 @@ async function getData(){
                     document.querySelector("#temp").append(temp);
                     document.querySelector("#tmax").append(tmax);
                     document.querySelector("#tmin").append(tmin);
+                    clearInp();
                 })
                 .catch(function(error){
                     document.querySelector(".error").append("No se pudo obtener informacion " + error);
